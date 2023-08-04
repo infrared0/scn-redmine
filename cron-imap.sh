24982a9fb35793d97139ef6380ccadc6c578824b
Paul Philion <philion@acmerocket.com>
2023-08-04 13:34:29 -0700

    more crontab tweaks, to see if I can get them running

--- cron-imap.sh
+++ cron-imap.sh
@@ -4,7 +4,7 @@
 ## on the redmine server to update issues from rmail.
 ##
 ## install with: 
-##     echo "* * * * * /opt/bitnami/redmine/cron-imap.sh" > /etc/crontabs/root
+##   echo "*/5 * * * * root /opt/bitnami/redmine/cron-imap.sh >> /opt/bitnami/redmine/log/cron.log" | crontab -
 
 cd /opt/bitnami/redmine
 bundle exec rake redmine:email:receive_imap --trace RAILS_ENV="production" host=imap.gmail.com port=993 username=redmine@seattlecommunitynetwork.org password=$REDMINE_IMAP_PASSWORD ssl=true project=scn unknown_user=accept >> /opt/bitnami/redmine/log/imap.log 2>&1
