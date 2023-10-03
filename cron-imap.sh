f522ebdd479343a60e9a28d7c476568a210b17ba
Paul Philion <pphlilion@istreamplanet.com>
2023-10-03 11:38:17 -0700

    Adding unknown_user=create param, as per ticket #69

--- cron-imap.sh
+++ cron-imap.sh
@@ -7,4 +7,4 @@
 ##   echo "*/5 * * * * root /opt/bitnami/redmine/cron-imap.sh >> /opt/bitnami/redmine/log/cron.log" | crontab -
 
 cd /opt/bitnami/redmine
-bundle exec rake redmine:email:receive_imap --trace RAILS_ENV="production" host=imap.gmail.com port=993 username=redmine@seattlecommunitynetwork.org password=$REDMINE_IMAP_PASSWORD ssl=true project=scn unknown_user=accept no_permission_check=1 >> /opt/bitnami/redmine/log/imap.log 2>&1
+bundle exec rake redmine:email:receive_imap --trace RAILS_ENV="production" host=imap.gmail.com port=993 username=redmine@seattlecommunitynetwork.org password=$REDMINE_IMAP_PASSWORD ssl=true project=scn unknown_user=create no_permission_check=1 >> /opt/bitnami/redmine/log/imap.log 2>&1
